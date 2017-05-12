9f535326808d6bb21543c98dae33e2d89941d748
Miguel Sequeiros <miguel.sequerios.net@gmail.com>
2017-05-12 17:01:49 -0500

    faltó una coma en el sttings

--- untitled/settings.py
+++ untitled/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'webpage'
+    'webpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
